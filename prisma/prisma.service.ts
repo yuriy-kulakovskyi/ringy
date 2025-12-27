419a901ec9d9a20fdad2c0ffbc9e6df4f5fb56c3
yuriy-kulakovskyi <yurakulak42@gmail.com>
2025-12-27 17:31:49 +0200

    feat: sql queries monitoring

--- prisma/prisma.service.ts
+++ prisma/prisma.service.ts
@@ -7,6 +7,18 @@ import { env } from "@config/env";
 const connectionString = env.DATABASE_URL;
 
 const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+
+const prisma = new PrismaClient({ adapter, log: [
+  { level: 'query', emit: 'event' },
+  { level: 'info', emit: 'event' },
+  { level: 'warn', emit: 'event' },
+  { level: 'error', emit: 'event' },
+] });
+
+prisma.$on('query', (e) => {
+  if (e.duration > 200) {
+    console.warn(`Slow query detected: ${e.query} (${e.duration}ms)`);
+  }
+});
 
 export default prisma;
