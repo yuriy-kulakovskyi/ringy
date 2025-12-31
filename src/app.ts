466f9912b4456aa113d057a9fc50b046339846ca
yuriy-kulakovskyi <yurakulak42@gmail.com>
2025-12-31 14:21:09 +0200

    refactor: reminder cron moved to the infratsructure

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import "reflect-metadata";
 import express from "express";
 
 import { httpLogger } from "@shared/logger/http-logger";
-import { startReminderCron } from "@modules/cal/application/cron/reminder.cron";
+import { startReminderCron } from "@infrastructure/cron/reminder.cron";
 
 export const app = express()
 app.use(express.json());
